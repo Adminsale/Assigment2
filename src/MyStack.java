fc281bc64b2b9465e6db58a19bfae08025e81be4
turarbekmarat <turarbekmarat896@gmail.com>
2025-04-14 00:20:26 +0600

    COMMIT

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,17 +1,22 @@
 
 public class MyStack <T extends Comparable<T>> {
+
     private final MyArrayList<T> list = new MyArrayList<>();
+    // Adds an item to the top of the stack
     public void push(T item) {
         list.addLast(item);
     }
+    // Removes and returns the top item of the stack
     public T pop() {
         T item = list.getLast();
         list.removeLast();
         return item;
     }
+    // Returns the top item without removing it
     public T peek() {
         return list.getLast();
     }
+    // Returns true if the stack is empty
     public boolean isEmpty() {
         return list.size() == 0;
     }
